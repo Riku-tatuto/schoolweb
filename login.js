a59c5162c7eb44c5d7d6c7b5d05e5a9a9b3e1b4f
Riku-tatuto <Riku_tatuto@outlook.com>
2025-04-18 17:38:57 +0900

    Update login.js

--- login.js
+++ login.js
@@ -5,12 +5,12 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 
 // 🔧 Firebase設定（あなたのプロジェクトに置き換えてね）
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXX",
-  appId: "APP_ID"
+  apiKey: "AIzaSyA7zF6AG8DutMOe2PZWmr3aGZU9RhsU9-A",
+  authDomain: "schoolweb-db.firebaseapp.com",
+  projectId: "schoolweb-db",
+  storageBucket: "schoolweb-db.firebasestorage.app",
+  messagingSenderId: "324683464267",
+  appId: "1:324683464267:web:f3a558fa58069c8cd397ce"
 };
 
 const app = initializeApp(firebaseConfig);
